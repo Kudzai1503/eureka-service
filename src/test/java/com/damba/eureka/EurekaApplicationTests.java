0f6e2813e2dd1da65c6621e677498660840ba672
Kudzai Damba <60918777+Kudzai1503@users.noreply.github.com>
2025-04-22 23:36:30 +0200

    -added management endpoints in config file

---
+++ src/test/java/com/damba/eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.damba.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Test method to check if spring context loads
+	}
+
+}
